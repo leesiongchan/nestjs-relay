453f19ec5e336d92f6736889b4f466e074dfe945
Lee Siong Chan <leesiongchan@outlook.com>
2021-04-30 18:58:29 +0800

    build: build

--- dist/cjs/global-object-identification/node/node-field.resolver.d.ts
+++ dist/cjs/global-object-identification/node/node-field.resolver.d.ts
@@ -1,6 +1,6 @@
 import { NodeInterface } from './node.interface';
 import { ResolvedGlobalId } from '../global-id';
-export declare type ResolvedNode = Promise<NodeInterface> | NodeInterface | Promise<null> | null | Promise<undefined> | undefined;
+export declare type ResolvedNode = Promise<NodeInterface> | NodeInterface | Promise<null> | null | Promise<undefined> | undefined | any;
 export interface NodeResolver {
     resolveNode(id: ResolvedGlobalId): ResolvedNode;
 }
